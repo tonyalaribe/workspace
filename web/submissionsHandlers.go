60c84f36ba2d63414cc6b50d53d8454f74d88a99
Anthony Alaribe <anthonyalaribe@gmail.com>
2017-05-11 15:51:08 +0100

    Now able to process files updated in draft submissions. Especially items in the file widget.

--- web/submissionsHandlers.go
+++ web/submissionsHandlers.go
@@ -74,41 +74,29 @@ func NewFormSubmissionHandler(w http.ResponseWriter, r *http.Request) {
 
 	conf := config.Get()
 
+	//Get the form metadata
 	var formInfoByte []byte
-	conf.DB.Update(func(tx *bolt.Tx) error {
+	conf.DB.View(func(tx *bolt.Tx) error {
 		formMetaBucket := tx.Bucket([]byte(config.WORKSPACES_CONTAINER)).Bucket([]byte(workspaceID)).Bucket([]byte(config.FORMS_METADATA))
 
 		formInfoByte = formMetaBucket.Get([]byte(formID))
 
 		return nil
 	})
-
-	// formInfo := make(map[string]interface{})
-	// err = json.Unmarshal(formInfoByte, &formInfo)
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-
 	formInfo, err := gabs.ParseJSON(formInfoByte)
 	if err != nil {
 		log.Println(err)
 	}
-
 	log.Printf("%+v", formInfo)
 
 	// schema := formInfo["jsonschema"].(map[string]interface{})
 	schema := formInfo.Path("jsonschema")
 
 	for k, v := range submission.FormData {
-		log.Printf("key: %+v, value: %+v", k, v)
-		// schemaObject := schema["properties"].(map[string]interface{})[k].(map[string]interface{})
 		schemaObject := schema.Path("properties").Search(k)
-		log.Println(schemaObject)
 
 		switch schemaObject.Path("type").Data().(string) {
 		case "string":
-			log.Println("processing a string type")
-			log.Println(schemaObject.Path("format"))
 
 			itemFormat := ""
 			// if schemaObject["format"] != nil {
@@ -120,7 +108,6 @@ func NewFormSubmissionHandler(w http.ResponseWriter, r *http.Request) {
 			switch itemFormat {
 			case "data-uri", "data-url":
 				//file formatting
-				log.Println("data-uri processing")
 				pathToSubmission := filepath.Join(conf.RootDirectory, username, submission.SubmissionName)
 				os.MkdirAll(pathToSubmission, os.ModePerm)
 				fullPath := Base64ToFileSystem(v.(string), pathToSubmission)
@@ -151,29 +138,10 @@ func NewFormSubmissionHandler(w http.ResponseWriter, r *http.Request) {
 			//Using type float64 due to compiler complaints when handling integer types
 			submission.FormData[k] = submission.FormData[k].(float64)
 		default:
-			log.Printf("%+v", schemaObject)
 			submission.FormData[k] = submission.FormData[k].(string)
 		}
 	}
 
-	log.Println(submission)
-
-	// formData.CreatedBy = username
-	// formData.CreationDate = time.Now().Format(time.RFC1123)
-	//
-	//
-	// for i, file := range formData.Files {
-	// 	pathToUser := filepath.Join(conf.RootDirectory, username, formData.SubmissionName)
-	// 	os.MkdirAll(pathToUser, os.ModePerm)
-	// 	filepath := filepath.Join(pathToUser, file.Name)
-	// 	Base64ToFileSystem(file.File, filepath)
-	//
-	// 	formData.Files[i].File = ""
-	// 	formData.Files[i].Path = filepath
-	// 	formData.Files[i].CreatedBy = username
-	// 	formData.Files[i].UploadDate = time.Now().Format(time.RFC1123)
-	//
-	// }
 	conf.DB.Update(func(tx *bolt.Tx) error {
 		formMetaBucket := tx.Bucket([]byte(config.WORKSPACES_CONTAINER)).Bucket([]byte(workspaceID)).Bucket([]byte(config.FORMS_METADATA))
 
@@ -223,59 +191,108 @@ func NewFormSubmissionHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateSubmissionHandler(w http.ResponseWriter, r *http.Request) {
+	username := r.Context().Value("username").(string)
+
 	httprouterParams := r.Context().Value("params").(httprouter.Params)
 	workspaceID := httprouterParams.ByName("workspaceID")
 	formID := httprouterParams.ByName("formID")
+
 	submissionID, err := strconv.Atoi(httprouterParams.ByName("submissionID"))
 	if err != nil {
 		log.Println(err)
 	}
-	log.Println(submissionID)
 
-	submissionData := SubmissionData{}
+	newSubmission := SubmissionData{}
+	err = json.NewDecoder(r.Body).Decode(&newSubmission)
+	if err != nil {
+		log.Println(err)
+	}
+
+	//Get the previously updated data
+	oldSubmission := SubmissionData{}
 	conf := config.Get()
 	err = conf.DB.View(func(tx *bolt.Tx) error {
 		formBucket := tx.Bucket([]byte(config.WORKSPACES_CONTAINER)).Bucket([]byte(workspaceID)).Bucket([]byte(formID))
 
-		err = json.Unmarshal(formBucket.Get(itob(submissionID)), &submissionData)
+		err = json.Unmarshal(formBucket.Get(itob(submissionID)), &oldSubmission)
 		if err != nil {
 			log.Println(err)
 		}
-
 		return nil
 	})
 	if err != nil {
 		log.Println(err)
 	}
+	oldSubmission.Status = newSubmission.Status
+	oldSubmission.LastModified = newSubmission.LastModified
+	oldSubmission.FormData = newSubmission.FormData
 
-	formData := SubmissionData{}
-	err = json.NewDecoder(r.Body).Decode(&formData)
+	//Get the form meta data. SO
+	var formInfoByte []byte
+	conf.DB.View(func(tx *bolt.Tx) error {
+		formMetaBucket := tx.Bucket([]byte(config.WORKSPACES_CONTAINER)).Bucket([]byte(workspaceID)).Bucket([]byte(config.FORMS_METADATA))
+
+		formInfoByte = formMetaBucket.Get([]byte(formID))
+		return nil
+	})
+	formMetaData, err := gabs.ParseJSON(formInfoByte)
 	if err != nil {
 		log.Println(err)
 	}
 
-	submissionData.Status = formData.Status
-	submissionData.LastModified = formData.LastModified
-	submissionData.FormData = formData.FormData
+	schema := formMetaData.Path("jsonschema")
+	for k, v := range newSubmission.FormData {
 
-	// for i, file := range formData.Files {
-	// 	pathToUser := filepath.Join(conf.RootDirectory, username, formData.SubmissionName)
-	// 	os.MkdirAll(pathToUser, os.ModePerm)
-	// 	filepath := filepath.Join(pathToUser, file.Name)
-	// 	Base64ToFileSystem(file.File, filepath)
-	//
-	// 	oldFile := formData.Files[i]
-	// 	oldFile.File = ""
-	// 	oldFile.Path = filepath
-	// 	oldFile.CreatedBy = username
-	// 	oldFile.UploadDate = time.Now().Format(time.RFC1123)
-	// 	submissionData.Files = append(submissionData.Files, oldFile)
+		schemaObject := schema.Path("properties").Search(k)
+		log.Println(schemaObject)
 
-	// }
-	//
-	/*Save to boltdb*/
+		switch schemaObject.Path("type").Data().(string) {
+		case "string":
+			itemFormat := ""
+			// if schemaObject["format"] != nil {
+			// 	itemFormat = schemaObject["format"].(string)
+			// }
+			if schemaObject.ExistsP("format") {
+				itemFormat = schemaObject.Path("format").Data().(string)
+			}
+			switch itemFormat {
+			case "data-uri", "data-url":
+				//file formatting
+				pathToSubmission := filepath.Join(conf.RootDirectory, username, newSubmission.SubmissionName)
+				os.MkdirAll(pathToSubmission, os.ModePerm)
+				fullPath := Base64ToFileSystem(v.(string), pathToSubmission)
+				oldSubmission.FormData[k] = fullPath
+				break
+			default:
+				oldSubmission.FormData[k] = v.(string)
+			}
 
-	dataByte, err := json.Marshal(submissionData)
+		case "array":
+
+			switch schemaObject.Path("items.type").Data().(string) {
+			case "string":
+				switch schemaObject.Path("items.format").Data().(string) {
+				case "data-url":
+					items := []string{}
+					for _, item := range v.([]interface{}) {
+						log.Println("data-uri processing")
+						pathToSubmission := filepath.Join(conf.RootDirectory, username, newSubmission.SubmissionName)
+						os.MkdirAll(pathToSubmission, os.ModePerm)
+						fullPath := Base64ToFileSystem(item.(string), pathToSubmission)
+						items = append(items, fullPath)
+					}
+					oldSubmission.FormData[k] = items
+				}
+			}
+		case "integer":
+			//Using type float64 due to compiler complaints when handling integer types
+			oldSubmission.FormData[k] = newSubmission.FormData[k].(float64)
+		default:
+			oldSubmission.FormData[k] = newSubmission.FormData[k].(string)
+		}
+	}
+
+	dataByte, err := json.Marshal(oldSubmission)
 	if err != nil {
 		log.Println(err)
 	}
