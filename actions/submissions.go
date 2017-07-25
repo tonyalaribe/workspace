b79425116a35c3ebab8ccf89cc0ead2d20e29000
Anthony Alaribe <anthonyalaribe@gmail.com>
2017-07-25 09:01:15 +0100

    functional uploads. added .workspace.yaml to gitignore

--- actions/submissions.go
+++ actions/submissions.go
@@ -113,7 +113,7 @@ func UpdateSubmission(workspaceID, formID string, submissionIDString string, new
 			switch itemFormat {
 			case "data-uri", "data-url":
 				//file formatting
-				pathToItem, err := conf.FileManager.Save(workspaceID, formID, submission.SubmissionName, v.(string))
+				pathToItem, err := conf.FileManager.Save(workspaceID, formID, newSubmission.SubmissionName, v.(string))
 				if err != nil {
 					log.Println(err)
 				}
@@ -130,7 +130,7 @@ func UpdateSubmission(workspaceID, formID string, submissionIDString string, new
 				case "data-url":
 					items := []string{}
 					for _, item := range v.([]interface{}) {
-						pathToItem, err := conf.FileManager.Save(workspaceID, formID, submission.SubmissionName, item.(string))
+						pathToItem, err := conf.FileManager.Save(workspaceID, formID, newSubmission.SubmissionName, item.(string))
 						if err != nil {
 							log.Println(err)
 						}
